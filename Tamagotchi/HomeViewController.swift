63fc305c88bb244ae0e08381736e907e4b557772
prayanalog <prayanalog@gmail.com>
2017-07-20 19:44:45 +0900

    make main page and add function's image, not yet madden function

---
+++ Tamagotchi/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.swift
+//  Tamagotchi
+//
+//  Created by user on 2017. 7. 20..
+//  Copyright © 2017년 user. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var petImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
