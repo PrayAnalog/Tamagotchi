975f9f7aba690cac3ab02a4fbe5d6b6ad980c0d4
yseolk <yunaseol@kaist.ac.kr>
2017-07-21 00:14:49 +0900

    basic pong game

--- Tamagotchi/GameScene.swift
+++ Tamagotchi/GameScene.swift
@@ -15,13 +15,24 @@ class GameScene: SKScene {
     var enemy = SKSpriteNode()
     var main = SKSpriteNode()
     
+    var topLabel = SKLabelNode()
+    var btmLabel = SKLabelNode()
+    
+    var score = [Int]()
     
     override func didMove(to view: SKView) {
+        
+        startGame()
+        
+        topLabel = self.childNode(withName: "topLabel") as! SKLabelNode
+        btmLabel = self.childNode(withName: "btmLabel") as! SKLabelNode
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+        enemy.position.y = (self.frame.height / 2) - 50
         main = self.childNode(withName: "main") as! SKSpriteNode
+        main.position.y = (-self.frame.height / 2) + 50
         
-        ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        ball.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 10))
         
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
         
@@ -29,7 +40,30 @@ class GameScene: SKScene {
         border.restitution = 1
         
         self.physicsBody = border
+    }
+    
+    func startGame() {
+        score = [0,0]
+        topLabel.text = "\(score[1])"
+        btmLabel.text = "\(score[0])"
+    }
+    
+    func addScore(playerWhoWon: SKSpriteNode) {
         
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        if playerWhoWon == main {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 10))
+        }
+        else if playerWhoWon == enemy {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -10, dy: -10))
+            
+        }
+        
+        topLabel.text = "\(score[1])"
+        btmLabel.text = "\(score[0])"
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -50,6 +84,14 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
-        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
+        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.2))
+        
+        if ball.position.y <= main.position.y - 30 {
+            addScore(playerWhoWon: enemy)
+        }
+            
+        else if ball.position.y >= enemy.position.y + 30 {
+            addScore(playerWhoWon: main)
+        }
     }
 }
