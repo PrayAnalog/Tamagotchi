7966010f33c00baec87fcafd7117d5ba4f544667
prayanalog <prayanalog@gmail.com>
2017-07-20 20:42:31 +0900

    add eat animation, not function yet

--- Tamagotchi/HomeViewController.swift
+++ Tamagotchi/HomeViewController.swift
@@ -23,6 +23,31 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    private func eatAnimation() {
+        
+        let petImageNameDefault : String = "petEatAction"
+        var imageListArray: [UIImage] = []
+        
+        
+        for countValue in 0..<2 {
+            let strImageName: String = petImageNameDefault + String(countValue)
+            let image = UIImage(named: strImageName)
+            imageListArray.append(image!)
+        }
+        
+        self.petImageView.animationImages = imageListArray
+        self.petImageView.animationDuration = 1.0
+        self.petImageView.animationRepeatCount = 3
+        self.petImageView.startAnimating()
+    }
+    
+    @IBAction func eatAction(_ sender: UIButton) {
+        // have to insert status change function
+        
+        
+        // change image looks like animation
+        eatAnimation()
+    }
 
     /*
     // MARK: - Navigation
